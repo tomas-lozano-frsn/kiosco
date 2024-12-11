2c18ab1f5c0dcc1121467a743d1768d2dc72c99c
tomas-lozano-frsn <tlozano@frsn.utn.edu.ar>
2024-12-11 13:30:47 -0300

    se agrega el proyecto

---
+++ models/cliente/cliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_final.models.cliente
+{
+    internal class cliente
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id_cliente { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string nombre { get; set; } = null!;
+
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = null!;
+
+        [Required]
+        public string UserName { get; set; } = null!;
+    }
+}
